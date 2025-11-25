b9e8cbfe49e7b4991a90f61cefa2d9bc5fb4c34c
bcherng <briancherngsch@gmail.com>
2025-11-24 16:30:21 -0800

    update linux package structure

--- src/windows/packaging/install_dependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing Python dependencies for FIM Daemon...
-python -m pip install watchdog
-if %ERRORLEVEL% EQU 0 (
-    echo Dependencies installed successfully.
-) else (
-    echo Failed to install dependencies.
-    pause
-)
